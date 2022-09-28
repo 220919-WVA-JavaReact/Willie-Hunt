3a2fb6032d77f88dee501b9293e39623f474895e
whunt12 <blackohma12@gmail.com>
2022-09-28 15:37:54 -0500

    SQL update

--- revature/assignments/week-2/week-2Assignment.sql
+++ revature/assignments/week-2/week-2Assignment.sql
@@ -1,41 +1,69 @@
 CREATE TABLE students (
-id serial PRIMARY KEY,
-"name" varchar(50),
+student_id serial PRIMARY KEY,
+"first" varchar(50),
+"last" varchar(50),
 email varchar(100)
 );
 
 CREATE TABLE teachers (
-id serial PRIMARY KEY,
-"name" varchar(50),
+teacher_id serial PRIMARY KEY,
+"first" varchar(50),
+"last" varchar(50),
 email varchar(100)
 );
 
-CREATE TABLE courses (
-courseNum int PRIMARY KEY,
-title varchar(25),
-slots int REFERENCES students,
-teacher_id int REFERENCES teachers UNIQUE
-);
-
 CREATE TABLE topics (
-"section_number" int PRIMARY KEY,
-"section_name" varchar(50)
+topic_id serial PRIMARY KEY,
+"content" text
 );
 
-INSERT INTO students 
+CREATE TABLE courses (
+course_id serial PRIMARY KEY,
+title varchar(50),
+course_number int,
+teacher_id int REFERENCES teachers unique
+);
+
+CREATE TABLE course_topic (
+course_id int REFERENCES courses,
+topic_id int REFERENCES topics,
+student_id int REFERENCES students
+);
+
+
+INSERT INTO students("first", "last", email) 
 VALUES 
-(1, 'Timmy johnson', 'tjohnson@aow.net'),
-(2, 'Bob Ray', 'billybob@yahoo.com'),
-(3, 'Chris Redfield', 'thatguy@gmail.com'),
-(4, 'Kendra Wilson', 'kwil995@hotmail.com'),
-(5, 'Lebron James', 'kingjames@notmyking.org');
+('Barry', 'Allan', 'speedster220@gmail.com'),
+('Bruce', 'Wayne', 'iambatman@gmail.net'),
+('Clack', 'Kent', 'manofsteel@gmail.com'),
+('Jon', 'Jones', 'thelanturn@gmail.com'),
+('Diana', 'Prince', 'amazonwoman@gmail.com');
 
-INSERT INTO teachers 
+INSERT INTO teachers ("first", "last", email)
 VALUES 
-(1, 'Professor Oak', 'gottocatchthemall@gen1.net'),
-(2, 'Profesor Zoom', 'barryhater@hahaha.com'),
-(3, 'Professor Bill', 'thescienceguy@billbillbill.net');
+('Steve', 'Strange', 'thedoctor@yahoo.com'),
+('Steve', 'Rogers', 'capmerica@aow.com'),
+('Bruce', 'Banner', 'greenmachine@yahoo.com');
 
-INSERT INTO courses
+INSERT INTO topics ("content")
+VALUES
+('Basic fighting styles'),
+('Intro into magic'),
+('Basic understanding of gamma radiation');
 
+INSERT INTO courses (title, course_number, teacher_id)
+VALUES 
+('Combat', 101, 2),
+('Arcane Arts', 450, 1),
+('Gamma Radiation', 400, 3);
+
+
+INSERT INTO course_topic 
+VALUES 
+(1, 1, 1),
+(1, 1, 5),
+(2, 2, 4),
+(2, 2, 5),
+(3, 3, 2),
+(3, 3, 1);
 
